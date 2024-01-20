9b401d62eea55927ab6fcdafce785e8dfacbe498
h-Soares <hiagosoares002@gmail.com>
2024-01-20 13:19:23 -0300

    project init

---
+++ src/main/java/com/soaresdev/uploaddownloadapi/UploadDownloadApiApplication.java
@@ -0,0 +1,13 @@
+package com.soaresdev.uploaddownloadapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UploadDownloadApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UploadDownloadApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/soaresdev/uploaddownloadapi/UploadDownloadApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soaresdev.uploaddownloadapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UploadDownloadApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
