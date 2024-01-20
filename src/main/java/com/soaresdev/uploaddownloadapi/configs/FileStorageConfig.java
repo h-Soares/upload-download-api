8d55787cc0a8bdff3551f3c38656382d4dca49e1
h-Soares <hiagosoares002@gmail.com>
2024-01-20 13:45:26 -0300

    FileStorageConfig created

---
+++ src/main/java/com/soaresdev/uploaddownloadapi/configs/FileStorageConfig.java
@@ -0,0 +1,14 @@
+package com.soaresdev.uploaddownloadapi.configs;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties(prefix = "file")
+public class FileStorageConfig {
+    private String uploadDirectory;
+
+    public String getUploadDirectory() {
+        return uploadDirectory;
+    }
+}
